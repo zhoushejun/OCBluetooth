10d6f19f98d3982279dd9e1180bdf932fac98fc9
周社军 <965678322@qq.com>
2014-10-21 21:23:36 +0800

    Initial Commit

---
+++ OCBluetooth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCBluetooth
+//
+//  Created by zhoushejun on 14-10-21.
+//  Copyright (c) 2014年 shejun.zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCBluetooth/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCBluetooth
+//
+//  Created by zhoushejun on 14-10-21.
+//  Copyright (c) 2014年 shejun.zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
