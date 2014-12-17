b4174839c8c0842c71e6f1e377960a8de0070a17
shejun.zhou <shejun.zhou@gajah.com.hk>
2014-12-17 09:21:29 +0800

    添加解析从手环返回来的数据的类

---
+++ OCBluetooth/OCHandBandDataModel.m
@@ -0,0 +1,13 @@
+//
+//  OCHandBandDataModel.m
+//  OCBluetooth
+//
+//  Created by shejun.zhou on 14/12/17.
+//  Copyright (c) 2014年 shejun.zhou. All rights reserved.
+//
+
+#import "OCHandBandDataModel.h"
+
+@implementation OCHandBandDataModel
+
+@end
